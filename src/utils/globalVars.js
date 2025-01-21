afc9388e2a27c960aced8813c6c4292cb97595af
HwlloChen <hwllochen@qq.com>
2025-01-21 21:05:03 +0800

    更新README.md以更好地描述项目，调整默认的主题设置位置

--- src/utils/globalVars.js
+++ src/utils/globalVars.js
@@ -18,11 +18,11 @@ const globalVars = reactive({
         ipv6: true,
         login: null,
     },
-    theme: { // 默认主题设置在init.js中
-        light: undefined,
-        color: undefined,
-        bgImage: undefined,
-        useGlass: undefined,
+    theme: { // 默认主题设置
+        light: "dark",
+        color: "#394b6b",
+        bgImage: "https://chenserver.top/files/img/mc/2022-summer_je_2.png",
+        useGlass: window.screen.width >= 1080 ? true : false, // 根据屏幕宽度决定
     },
     author: {
         name: "Chen",
@@ -31,7 +31,7 @@ const globalVars = reactive({
     site: {
         name: "ChenServer",
         created_date: "2022-10-09T00:00:00", // ISO8601格式日期字符串，记录网站创建时间, 胡填代表不使用
-        backpoint: 'https://v4.chenserver.top:11450/api',
+        backpoint: 'https://v4.chenserver.top/api',
         backpoint_v6: "https://www.chenserver.top:81",
     },
     artalk: {
--- src/utils/init.js
+++ src/utils/init.js
@@ -6,17 +6,11 @@ import { Announcements } from "./Announcements";
  * Init Theme
  * 初始化主题配置
  */
-// 默认主题配置
-const defaultTheme = {
-    light: "dark",
-    color: "#394b6b",
-    bgImage: "https://chenserver.top/files/img/mc/2022-summer_je_2.png",
-    useGlass: window.screen.width >= 1080 ? true : false, // 根据屏幕宽度决定
-}
+
 if (localStorage.getItem("theme") == null) {
     //预创建默认主题配置
     console.log("创建主题配置")
-    localStorage.setItem("theme", JSON.stringify(defaultTheme))
+    localStorage.setItem("theme", JSON.stringify(globalVars.theme))
 }
 // 加载主题配置
 globalVars.theme = JSON.parse(localStorage.getItem("theme"))
